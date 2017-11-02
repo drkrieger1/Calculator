531578e5a0dd9fb20ac11c8b958e579d51525f57
drkrieger <eplyushko@gmail.com>
2017-11-02 16:19:38 -0700

    working through the number function

--- Calculator/wwwroot/js/app.js
+++ Calculator/wwwroot/js/app.js
@@ -1,18 +1,20 @@
-﻿
+﻿"use strict";
 //ajax test
 
-var A ="5";
-var B = "6"; 
-var opt = "+";
+var A = '';
+var B = ''; 
+var opt = '';
 var answer;
 
-
 var button = document.getElementById('send');
+var clear = document.getElementById('clear');
 
-button.addEventListener('click', Payload)
+button.addEventListener('click', Payload);
+clear.addEventListener('click', Clear);
 
 function Payload() {
     console.log('launching payload');
+    
     $.ajax({
         type: 'POST',
         url: '/Home/Calc',
@@ -27,12 +29,68 @@ function Payload() {
     function OnError() {
         console.log(error);
     }
+    
 }
 
 function Answer() {
-    $.get("/Home/Return", function(payload){
+    $.get("/Home/Return", function (payload) {
         console.log(`answer payload returned: ${payload}`);
-        answer = parseInt(payload);
+        answer = payload;
+        render(payload);
     });
 }
 
+function render(input) {
+    $('#Print').val('');
+    $('#Print').text(input);
+}
+
+function AddToA(num) {
+    if (A === '') {
+        A = num;
+        render(A);
+    } else {
+        let Acopy = A;
+        A = Acopy + num;
+        render(A);
+    }
+}
+
+function AddToB(num) {
+    if (B === '') {
+        B = num;
+        render(A);
+    } else {
+        let Bcopy = B;
+        B = Bcopy + num;
+        render(B);
+    }
+}
+
+function ButtonEvent() {
+    
+    $(".bt").click(function () {
+        //if operator is not present fill Add to A
+        var input = $(this).attr('value');
+        console.log(input);
+        if (input === '/' || input === '*' || input === '-' || input === '+' && input !== '='){
+            opt = input;
+        }
+        if (opt === ''){
+           console.log(input);
+           AddToA(input);
+        }else {
+            AddToB(input); 
+        }    
+
+    });
+}
+    function Clear() {
+        $('#Print').val('');
+        A = '';
+        B = '';
+        opt = '';
+        render('0');
+    }
+
+$(document).ready(ButtonEvent);
